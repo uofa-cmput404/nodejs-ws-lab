e720c958219f6364a419cb21834ac902566869e7
amahesh <amahesh@ntwist.com>
2021-11-18 22:16:38 -0700

    lab 8 done

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/** @format */
 
 const { Server: WebSocketServer } = require("ws");
 const express = require("express");
@@ -13,74 +14,79 @@ const STATIC_DIR = "static";
  * Build and minify all client code for Express
  */
 function bundleClient() {
-  const options = {
-    outDir: PUBLIC_DIR,
-    contentHash: true,
-    bundleNodeModules: true,
-    watch: false,
-    detailedReport: true
-  };
-  const bundler = new ParcelBundler("index.html", options);
-  return bundler.bundle();
+	const options = {
+		outDir: PUBLIC_DIR,
+		contentHash: true,
+		bundleNodeModules: true,
+		watch: false,
+		detailedReport: true,
+	};
+	const bundler = new ParcelBundler("index.html", options);
+	return bundler.bundle();
 }
 
 /**
  * Setup a quick Web Socket server
  */
 function setupWSServer(server) {
-  const wss = new WebSocketServer({
-    server,
-    autoAcceptConnections: false
-  });
-  let actorCoordinates = { x: 100, y: 100 };
-  wss.on("connection", (ws) => {
-    ws.on("message", (rawMsg) => {
-      console.log(`RECV: ${rawMsg}`);
-      const incommingMessage = JSON.parse(rawMsg);
-      actorCoordinates.x = incommingMessage.x;
-      actorCoordinates.y = incommingMessage.y;
-      wss.clients.forEach((wsClient) => {
-        wsClient.send(JSON.stringify(actorCoordinates));
-      })
-    });
-    ws.send(JSON.stringify(actorCoordinates));
-  });
-  wss.on("listening", () => {
-    const addr = server.address();
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-    console.log(`WebSocketServer listening on ${bind}`);
-  });
-  return wss;
+	const wss = new WebSocketServer({
+		server,
+		autoAcceptConnections: false,
+	});
+	let actorCoordinates = {};
+	wss.on("connection", (ws) => {
+		ws.on("message", (rawMsg) => {
+			console.log(`RECV: ${rawMsg}`);
+			const incommingMessage = JSON.parse(rawMsg);
+			actorCoordinates[incommingMessage.id] = {
+				x: incommingMessage.x,
+				y: incommingMessage.y,
+				frame: incommingMessage.frame,
+			};
+			wss.clients.forEach((wsClient) => {
+				wsClient.send(JSON.stringify(actorCoordinates));
+			});
+		});
+		ws.send(JSON.stringify(actorCoordinates));
+	});
+	wss.on("listening", () => {
+		const addr = server.address();
+		const bind =
+			typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+		console.log(`WebSocketServer listening on ${bind}`);
+	});
+	return wss;
 }
 
 /**
  * Setup an Express application and web server
  */
 function setupServer() {
-  // Setup the Express application
-  const app = express();
-  app.set("port", PORT)
-  app.use("/", express.static(PUBLIC_DIR))
-  app.use("/static", express.static(STATIC_DIR))
+	// Setup the Express application
+	const app = express();
+	app.set("port", PORT);
+	app.use("/", express.static(PUBLIC_DIR));
+	app.use("/static", express.static(STATIC_DIR));
 
-  // Setup the HTTP Web Server
-  const server = http.createServer(app);
-  server.listen(PORT);
-  server.on("listening", () => {
-    const addr = server.address();
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-    console.log(`WebServer listening on ${bind}`);
-  });
-  server.on("error", (err) => {
-    console.error(err);
-    process.exit(1);
-  });
-  return server;
+	// Setup the HTTP Web Server
+	const server = http.createServer(app);
+	server.listen(PORT);
+	server.on("listening", () => {
+		const addr = server.address();
+		const bind =
+			typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+		console.log(`WebServer listening on ${bind}`);
+	});
+	server.on("error", (err) => {
+		console.error(err);
+		process.exit(1);
+	});
+	return server;
 }
 
 if (require.main === module) {
-  bundleClient().then(() => {
-    const server = setupServer();
-    setupWSServer(server);
-  });
+	bundleClient().then(() => {
+		const server = setupServer();
+		setupWSServer(server);
+	});
 }
