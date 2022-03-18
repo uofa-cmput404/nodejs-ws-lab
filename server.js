cce04c6b8957d25c5becaa38ef5bd7f4e913191f
Bo Lun Zhang <bolun1@cs.ualberta.ca>
2022-03-18 11:41:49 -0600

    done

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const { Server: WebSocketServer } = require("ws");
-const express = require("express");
-const http = require("http");
-const ParcelBundler = require("parcel-bundler");
+const { Server: WebSocketServer } = require('ws');
+const express = require('express');
+const http = require('http');
+const ParcelBundler = require('parcel-bundler');
 
-const PORT = process.env.PORT || 8080;
-const PUBLIC_DIR = "public";
-const STATIC_DIR = "static";
+const PORT = process.env.PORT || 8000;
+const PUBLIC_DIR = 'public';
+const STATIC_DIR = 'static';
 
 /**
  * Build and minify all client code for Express
@@ -18,9 +18,9 @@ function bundleClient() {
     contentHash: true,
     bundleNodeModules: true,
     watch: false,
-    detailedReport: true
+    detailedReport: true,
   };
-  const bundler = new ParcelBundler("index.html", options);
+  const bundler = new ParcelBundler('index.html', options);
   return bundler.bundle();
 }
 
@@ -30,24 +30,30 @@ function bundleClient() {
 function setupWSServer(server) {
   const wss = new WebSocketServer({
     server,
-    autoAcceptConnections: false
+    autoAcceptConnections: false,
   });
   let actorCoordinates = { x: 100, y: 100 };
-  wss.on("connection", (ws) => {
-    ws.on("message", (rawMsg) => {
+  wss.on('connection', (ws) => {
+    ws.on('message', (rawMsg) => {
       console.log(`RECV: ${rawMsg}`);
       const incommingMessage = JSON.parse(rawMsg);
-      actorCoordinates.x = incommingMessage.x;
-      actorCoordinates.y = incommingMessage.y;
+      // actorCoordinates.x = incommingMessage.x;
+      // actorCoordinates.y = incommingMessage.y;
+      actorCoordinates[incommingMessage.id] = {
+        x: incommingMessage.x,
+        y: incommingMessage.y,
+        frame: incommingMessage.frame,
+      };
       wss.clients.forEach((wsClient) => {
         wsClient.send(JSON.stringify(actorCoordinates));
-      })
+      });
     });
     ws.send(JSON.stringify(actorCoordinates));
   });
-  wss.on("listening", () => {
+  wss.on('listening', () => {
     const addr = server.address();
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+    const bind =
+      typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
     console.log(`WebSocketServer listening on ${bind}`);
   });
   return wss;
@@ -59,19 +65,20 @@ function setupWSServer(server) {
 function setupServer() {
   // Setup the Express application
   const app = express();
-  app.set("port", PORT)
-  app.use("/", express.static(PUBLIC_DIR))
-  app.use("/static", express.static(STATIC_DIR))
+  app.set('port', PORT);
+  app.use('/', express.static(PUBLIC_DIR));
+  app.use('/static', express.static(STATIC_DIR));
 
   // Setup the HTTP Web Server
   const server = http.createServer(app);
   server.listen(PORT);
-  server.on("listening", () => {
+  server.on('listening', () => {
     const addr = server.address();
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+    const bind =
+      typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
     console.log(`WebServer listening on ${bind}`);
   });
-  server.on("error", (err) => {
+  server.on('error', (err) => {
     console.error(err);
     process.exit(1);
   });
