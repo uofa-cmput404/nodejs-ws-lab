930e2bf41f0031f5e3be75fc557f8b9735ae3553
Brad Conklin <bconklin@ualberta.ca>
2022-11-18 15:48:27 -0800

    lab8

--- client.ts
+++ client.ts
@@ -4,8 +4,11 @@
 import * as Phaser from "phaser";
 
 interface ICoords {
-  x: number;
-  y: number;
+  [key: string]: {
+    x: number;
+    y: number;
+    frame: number;
+  }
 }
 
 const DEBUG = false; // Render debug physics entities
@@ -16,13 +19,17 @@ class GameScene extends Phaser.Scene {
 
   private VELOCITY = 100;
   private wsClient?: WebSocket;
-  private player?: Phaser.GameObjects.Sprite;
+  //private player?: Phaser.GameObjects.Sprite;
+  private id = uuid();
+  private players: { [key: string]: Phaser.GameObjects.Sprite } = {};
   private leftKey?: Phaser.Input.Keyboard.Key;
   private rightKey?: Phaser.Input.Keyboard.Key;
   private upKey?: Phaser.Input.Keyboard.Key;
   private downKey?: Phaser.Input.Keyboard.Key;
 
-  constructor() { super({ key: "GameScene" }); }
+  constructor() {
+    super({key: "GameScene"});
+  }
 
   /**
    * Load the assets required by the scene
@@ -44,9 +51,33 @@ class GameScene extends Phaser.Scene {
     this.wsClient.onopen = (event) => console.log(event);
     // TODO: multiplayer functionality
     this.wsClient.onmessage = (wsMsgEvent) => {
-      console.log(wsMsgEvent)
+      const allCoords: ICoords = JSON.parse(wsMsgEvent.data);
+      for (const playerId of Object.keys(allCoords)) {
+        if (playerId === this.id) {
+          // we don't need to update ourselves
+          continue;
+        }
+        const {x, y, frame} = allCoords[playerId];
+        if (playerId in this.players) {
+          // We have seen this player before, update it!
+          const player = this.players[playerId];
+          if (player.texture.key === "__MISSING") {
+            // Player was instantiated before texture was ready, reinstantiate
+            player.destroy();
+            this.players[playerId] = this.add.sprite(x, y, "player", frame);
+          } else {
+            player.setX(x);
+            player.setY(y);
+            player.setFrame(frame);
+          }
+        } else {
+          // We have not seen this player before, create it!
+          this.players[playerId] = this.add.sprite(x, y, "player", frame);
+        }
+      }
     }
   }
+
 
   /**
    * Create the game objects required by the scene
@@ -64,35 +95,35 @@ class GameScene extends Phaser.Scene {
     // Player animations
     this.anims.create({
       key: "left",
-      frames: this.anims.generateFrameNumbers("player", { start: 8, end: 9 }),
+      frames: this.anims.generateFrameNumbers("player", {start: 8, end: 9}),
       frameRate: 10,
       repeat: -1
     });
     this.anims.create({
       key: "right",
-      frames: this.anims.generateFrameNumbers("player", { start: 1, end: 2 }),
+      frames: this.anims.generateFrameNumbers("player", {start: 1, end: 2}),
       frameRate: 10,
       repeat: -1
     });
     this.anims.create({
       key: "up",
-      frames: this.anims.generateFrameNumbers("player", { start: 11, end: 13 }),
+      frames: this.anims.generateFrameNumbers("player", {start: 11, end: 13}),
       frameRate: 10,
       repeat: -1
     });
     this.anims.create({
       key: "down",
-      frames: this.anims.generateFrameNumbers("player", { start: 4, end: 6 }),
+      frames: this.anims.generateFrameNumbers("player", {start: 4, end: 6}),
       frameRate: 10,
       repeat: -1
     });
 
     // Player game object
-    this.player = this.physics.add.sprite(48, 48, "player", 1);
-    this.physics.add.collider(this.player, layer);
-    this.cameras.main.startFollow(this.player);
+    this.players[this.id] = this.physics.add.sprite(48, 48, "player", 1);
+    this.physics.add.collider(this.players[this.id], layer);
+    this.cameras.main.startFollow(this.players[this.id]);
     this.cameras.main.setBounds(
-      0, 0, tileMap.widthInPixels, tileMap.heightInPixels
+        0, 0, tileMap.widthInPixels, tileMap.heightInPixels
     );
 
     // Keyboard input bindings
@@ -103,39 +134,54 @@ class GameScene extends Phaser.Scene {
   }
 
   public update() {
-    if (this.player) {
-      let moving = false;
-      if (this.leftKey && this.leftKey.isDown) {
-        (this.player.body as Phaser.Physics.Arcade.Body).setVelocityX(-this.VELOCITY);
-        this.player.play("left", true);
-        moving = true;
-      } else if (this.rightKey && this.rightKey.isDown) {
-        (this.player.body as Phaser.Physics.Arcade.Body).setVelocityX(this.VELOCITY);
-        this.player.play("right", true);
-        moving = true;
-      } else {
-        (this.player.body as Phaser.Physics.Arcade.Body).setVelocityX(0);
+    for (const playerId of Object.keys(this.players)) {
+      const player = this.players[playerId];
+
+      if (playerId !== this.id) {
+        player.setTint(0x0000aa); // so we can tell our guy apart
+        player.update();
+        continue;
       }
-      if (this.upKey && this.upKey.isDown) {
-        (this.player.body as Phaser.Physics.Arcade.Body).setVelocityY(-this.VELOCITY);
-        this.player.play("up", true);
-        moving = true;
-      } else if (this.downKey && this.downKey.isDown) {
-        (this.player.body as Phaser.Physics.Arcade.Body).setVelocityY(this.VELOCITY);
-        this.player.play("down", true);
-        moving = true;
-      } else {
-        (this.player.body as Phaser.Physics.Arcade.Body).setVelocityY(0);
+      if (this.players[this.id]) {
+        let moving = false;
+        if (this.leftKey && this.leftKey.isDown) {
+          (this.players[this.id].body as Phaser.Physics.Arcade.Body).setVelocityX(-this.VELOCITY);
+          this.players[this.id].play("left", true);
+          moving = true;
+        } else if (this.rightKey && this.rightKey.isDown) {
+          (this.players[this.id].body as Phaser.Physics.Arcade.Body).setVelocityX(this.VELOCITY);
+          this.players[this.id].play("right", true);
+          moving = true;
+        } else {
+          (this.players[this.id].body as Phaser.Physics.Arcade.Body).setVelocityX(0);
+        }
+        if (this.upKey && this.upKey.isDown) {
+          (this.players[this.id].body as Phaser.Physics.Arcade.Body).setVelocityY(-this.VELOCITY);
+          this.players[this.id].play("up", true);
+          moving = true;
+        } else if (this.downKey && this.downKey.isDown) {
+          (this.players[this.id].body as Phaser.Physics.Arcade.Body).setVelocityY(this.VELOCITY);
+          this.players[this.id].play("down", true);
+          moving = true;
+        } else {
+          (this.players[this.id].body as Phaser.Physics.Arcade.Body).setVelocityY(0);
+        }
+        if (!moving) {
+          (this.players[this.id].body as Phaser.Physics.Arcade.Body).setVelocity(0);
+          this.players[this.id].anims.stop();
+        } else if (this.wsClient) {
+          this.wsClient.send(JSON.stringify({
+            id: this.id,
+            x: this.players[this.id].x,
+            y: this.players[this.id].y,
+            frame: this.players[this.id].frame.name
+          }));
+        }
+        this.players[this.id].update();
       }
-      if (!moving) {
-        (this.player.body as Phaser.Physics.Arcade.Body).setVelocity(0);
-        this.player.anims.stop();
-      }
-      this.player.update();
     }
   }
 }
-
 
 // Phaser configuration variables
 const config: GameConfig = {
@@ -160,3 +206,27 @@ class LabDemoGame extends Phaser.Game {
 window.addEventListener("load", () => {
   new LabDemoGame(config);
 });
+
+
+function uuid(
+    a?: any               // placeholder
+): string {
+  return a              // if the placeholder was passed, return
+      ? (                 // a random number from 0 to 15
+          a ^               // unless b is 8,
+          Math.random()     // in which case
+          * 16              // a random number from
+          >> a / 4          // 8 to 11
+      ).toString(16)      // in hexadecimal
+      : (                 // or otherwise a concatenated string:
+          1e7.toString() +  // 10000000 +
+          -1e3 +            // -1000 +
+          -4e3 +            // -4000 +
+          -8e3 +            // -80000000 +
+          -1e11             // -100000000000,
+      ).replace(          // replacing
+          /[018]/g,         // zeroes, ones, and eights with
+          uuid              // random hex digits
+      )
+}
+
