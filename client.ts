4372bcdfce07ea4bb826e29d0fefe3c3aa598568
Davin <71451687+bc-davin@users.noreply.github.com>
2023-11-27 17:43:04 -0700

    done

--- client.ts
+++ client.ts
@@ -105,3 +105,25 @@ class LabDemoGame extends Phaser.Game {
 window.addEventListener("load", () => {
   new LabDemoGame(config);
 })
+
+function uuid(
+  a?: any               // placeholder
+): string {
+  return a              // if the placeholder was passed, return
+    ? (                 // a random number from 0 to 15
+      a ^               // unless b is 8,
+      Math.random()     // in which case
+      * 16              // a random number from
+      >> a / 4          // 8 to 11
+    ).toString(16)      // in hexadecimal
+    : (                 // or otherwise a concatenated string:
+      1e7.toString() +  // 10000000 +
+      -1e3 +            // -1000 +
+      -4e3 +            // -4000 +
+      -8e3 +            // -80000000 +
+      -1e11             // -100000000000,
+    ).replace(          // replacing
+      /[018]/g,         // zeroes, ones, and eights with
+      uuid              // random hex digits
+    )
+}
